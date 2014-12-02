22f2599e4ee304693c70a183ea0e00c0f6f6e3ef
Kunnapat <KunnapatRungruangsatien@Kunnapats-MacBook-Air.local>
2014-12-02 15:40:10 +0700

    Test cases added

--- lib/lovemac.rb
+++ lib/lovemac.rb
@@ -1,16 +1,26 @@
 def lovemac(input)
+	h =0
+	l =0
+	m =0
 	for i in 1..input
 		if i % 3 == 0
 			if i % 5 == 0
 				puts "HateWindows"
+				h = h + 1
 			else
 				puts "Love"
+				l = l + 1
 			end
 		elsif i % 5 == 0
  			puts "Mac"
+ 			m = m + 1
 		else
    			puts "#{i}" 
    		end
 	end
-	return input
+	puts ""
+	puts ""
+	puts ""
+	puts ""
+	return "("+l.to_s+","+m.to_s+","+h.to_s+")"
 end
--- spec/lib/lovemac_spec.rb
+++ spec/lib/lovemac_spec.rb
@@ -3,9 +3,27 @@ require "spec_helper"
 
 describe "#lovemac(number)" do
 	#Describe Method
-	it "Test if you love mac" do
+	it "Test if print how many number of word of Love, Mac, HateWindows are printed" do
+		#Describe method scenario
+		status = lovemac(3);
+		expect(status).to eq("(1,0,0)")
+	end
+
+	it "Test if print how many number of word of Love, Mac, HateWindows are printed" do
+		#Describe method scenario
+		status = lovemac(6);
+		expect(status).to eq("(2,1,0)")
+	end
+
+	it "Test if print how many number of word of Love, Mac, HateWindows are printed" do
+		#Describe method scenario
+		status = lovemac(15);
+		expect(status).to eq("(4,2,1)")
+	end
+
+	it "Test if print how many number of word of Love, Mac, HateWindows are printed" do
 		#Describe method scenario
 		status = lovemac(50);
-		expect(status).to eq(50)
+		expect(status).to eq("(13,7,3)")
 	end
 end
